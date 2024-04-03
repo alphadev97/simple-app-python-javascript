e9993f67b38ce2958a996277de601524e0ff2cf1
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-04-04 00:23:21 +0500

    CRUD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import ContactList from "./ContactList";
+import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentContact, setCurrentContact] = useState({});
 
   useEffect(() => {
     fetchContacts();
@@ -11,17 +14,52 @@ function App() {
 
   const fetchContacts = async () => {
     const response = await fetch("http://127.0.0.1:5000/contacts");
-    if (!response.ok) {
-      throw new Error("Failed to fetch contacts");
-    }
     const data = await response.json();
     setContacts(data.contact);
-    console.log(data.contact);
+    console.log(data);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCurrentContact({});
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true);
+  };
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return;
+    setCurrentContact(contact);
+    setIsModalOpen(true);
+  };
+
+  const onUpdate = () => {
+    closeModal();
+    fetchContacts();
   };
 
   return (
     <>
-      <ContactList contacts={contacts} />
+      <ContactList
+        contacts={contacts}
+        updateContact={openEditModal}
+        updateCallback={onUpdate}
+      />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm
+              existingContact={currentContact}
+              updateCallback={onUpdate}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 }
--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, updateContact, updateCallback }) => {
+  console.log(contacts);
+  const onDelete = async (id) => {
+    try {
+      const options = {
+        method: "DELETE",
+      };
+      const response = await fetch(
+        `http://127.0.0.1:5000/delete_contact/${id}`,
+        options
+      );
+      if (response.status === 200) {
+        updateCallback();
+      } else {
+        console.error("Failed to delete");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   // Check if contacts is undefined or empty
   if (!contacts || contacts.length === 0) {
     return <div>No contacts available</div>;
@@ -25,8 +45,8 @@ const ContactList = ({ contacts }) => {
               <td>{contact.lastName}</td>
               <td>{contact.email}</td>
               <td>
-                <button>Update</button>
-                <button>Delete</button>
+                <button onClick={() => updateContact(contact)}>Update</button>
+                <button onClick={() => onDelete(contact.id)}>Delete</button>
               </td>
             </tr>
           ))}
